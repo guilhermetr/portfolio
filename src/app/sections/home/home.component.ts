6607ec529e2aaeecfd512520ee8101cf9220d2f7
Guilherme <guilhermetarter@hotmail.com>
2023-12-20 14:05:30 -0300

    Add initial level 4

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  @Input() level!: number;
   
   constructor() {}
+
+  getLevelClass(): string {
+    return `level${this.level}`;
+  }
   
 }
